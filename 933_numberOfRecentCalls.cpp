17673b3ab759f78863813a18eac3fed0f6166622
xyp8023 <xyp8023@gmail.com>
2019-08-19 18:26:10 +0200

    add 933

---
+++ 933_numberOfRecentCalls.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <deque>
+
+using namespace std;
+class RecentCounter {
+public:
+    RecentCounter() {
+        deque<int> pings;
+    }
+    
+    int ping(int t) {
+        RecentCounter::RecentCounter();
+        
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
+
+int main(){
+    RecentCounter* obj = new RecentCounter();
+    int param_1 = obj->ping(t);
+}
