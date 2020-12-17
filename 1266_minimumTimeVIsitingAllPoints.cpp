e86bfc0fd45dd28f9f4cfd6c71928f55603ba42f
xyp8023 <xyp8023@gmail.com>
2020-12-17 12:01:07 +0100

    added 1266

---
+++ 1266_minimumTimeVIsitingAllPoints.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int times=0;
+        for (int i=1; i<points.size(); i++){
+            
+                times +=max(abs(points[i][0]-points[i-1][0]), abs(points[i][1]-points[i-1][1]));
+                
+            
+        }
+        return times;
+    }
+        
+};
